1d45c245bde9dc00eb5c7734704afea17b75289b
neil patel <neil@myindianweddingapp.com>
2018-08-30 10:15:35 +0100

    first commit

---
+++ network_details.js
@@ -0,0 +1,7 @@
+var network_details = {
+    address: 'that place where I put that thing that time',
+    network_name: 'Nick',
+    password_as_spoken: 'Capital Nick. 1. 2. 3. '
+}
+
+module.exports = network_details;
